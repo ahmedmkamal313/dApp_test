54d8f555bd90c44991e5c8fc38db8b74392573ff
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2024-11-15 01:32:40 +0200

    added some ui matrial

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final primaryColor = const Color(0xff1E3A8A);
+  final secondryColor = const Color(0xff3EC1D3);
+  final accentColor = const Color(0xffFF6F61);
+  final backgroundColor = const Color(0xffF0F8FF);
+  final whiteColor = const Color(0xffFFFFFF);
+  final blackColor = const Color(0xff000000);
+  final secondryTextColor = const Color(0xff000435);
+}
